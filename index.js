550a5ad3d46889043a50b2c02cc0e53d389f3741
krissanawat kaewsanmuang <me@kriss.io>
2020-03-26 23:36:31 +0700

    Chapter 1 - Splashscreen and Icon

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import React, { useEffect } from 'react';
+import { name as appName } from './app.json';
+import SplashScreen from 'react-native-splash-screen'
+const Apps = () => {
+    useEffect(() => {
+        SplashScreen.hide();
+    })
+    return <App />
+}
+AppRegistry.registerComponent(appName, () => Apps);
